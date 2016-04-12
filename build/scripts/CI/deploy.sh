4ab3015a6714552e7186d7ffa2be8e53c157560f
Andrew Howden <hello@andrewhowden.com>
2016-04-12 21:38:32 +1000

    Enable debugging in deploy.sh
    Need to see whats happening with Make, and why make can't find
    sassc

--- build/scripts/CI/deploy.sh
+++ build/scripts/CI/deploy.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-set -e
+set -ex
 
-# Sassc installs to ${HOME}/${BIN}. More generally, new binaries should probably
-# go here anyway.
-export PATH="${PATH}:${HOME}/bin"
-
-# Find all the changed containers
-CHANGED_CONTAINERS=$(git diff --name-only master | grep 'build\/docker' | sed -e 's/build\/docker\///' | sed -e 's/\/Dockerfile//');
+export PATH="${PATH}:$(pwd)/bin"
 
 make deploy-container-nginx
