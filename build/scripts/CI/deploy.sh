ef00ef35cd772c23bae7f5ef6a1672ff1d21d088
Andrew Howden <hello@andrewhowden.com>
2016-04-12 21:26:38 +1000

    Make ${HOME}/${BIN} part of the path for the make command

--- build/scripts/CI/deploy.sh
+++ build/scripts/CI/deploy.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+# Sassc installs to ${HOME}/${BIN}. More generally, new binaries should probably
+# go here anyway.
+export PATH="${PATH}:${HOME}/bin"
+
 # Find all the changed containers
 CHANGED_CONTAINERS=$(git diff --name-only master | grep 'build\/docker' | sed -e 's/build\/docker\///' | sed -e 's/\/Dockerfile//');
 
