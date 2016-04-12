b6a2f861c6b77f7e57accf65a8e7355438f6eb49
Andrew Howden <hello@andrewhowden.com>
2016-04-12 19:58:49 +1000

    Update unpack command

--- build/scripts/CI/unpack-secrets.sh
+++ build/scripts/CI/unpack-secrets.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Decrypt the tar file
-openssl aes-256-cbc -K $encrypted_1fa1ae5fd8c9_key -iv $encrypted_1fa1ae5fd8c9_iv -in .secrets.tar.gz.enc -out .secrets.tar.gz -d
+openssl aes-256-cbc -K $encrypted_33efbe80436a_key -iv $encrypted_33efbe80436a_iv -in .secrets.tar.gz.enc -out .secrets.tar.gz -d
 
 # Unpack the file
 tar -xvf .secrets.tar.gz
