a23412bda79fe9898368eb39c16ce1e42ca2fa56
Andrew Howden <hello@andrewhowden.com>
2016-04-13 23:06:52 +1000

    Remove nodejs, bower
    Because we're now committing the generated, cache busted files to the
    repo, we don't need to compile them as part of travis.
    
    We're committing them to the repo as we're deploying often enough that
    we don't want to regenerate them all the time.
      - It takes time, and
      - It busts caches for no reason
    
    Thus: We generate files as required, and commit them.

--- build/scripts/CI/install-dependencies.sh
+++ build/scripts/CI/install-dependencies.sh
@@ -32,7 +32,3 @@ google-cloud-sdk/install.sh --additional-components bq core gsutil gcloud alpha 
 wget "https://github.com/spf13/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_linux_amd64.tar.gz"
 tar -xvf hugo_${HUGO_VERSION}_linux_amd64.tar.gz
 mv hugo_${HUGO_VERSION}_linux_amd64/hugo_${HUGO_VERSION}_linux_amd64 ./bin/hugo
-
-# Install bower
-npm install -g bower
-bower install
