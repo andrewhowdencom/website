85eac72fce5e050687faaec029366a28a3848acb
Andrew Howden <hello@andrewhowden.com>
2018-07-28 19:06:00 +0200

    AD-HOC feat (helm): Install helm,

--- ci/scripts/install-dependencies.sh
+++ ci/scripts/install-dependencies.sh
@@ -3,6 +3,7 @@
 set -e
 
 GOOGLE_CLOUD_VERSION=206.0.0
+HELM_VERSION="2.9.1"
 PATH=$PATH:$(pwd)/bin
 
 # Defined in Travis
@@ -17,6 +18,11 @@ wget "https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sd
 tar -xvf google-cloud-sdk-${GOOGLE_CLOUD_VERSION}-linux-x86_64.tar.gz
 
 google-cloud-sdk/install.sh --additional-components bq core gsutil gcloud alpha beta kubectl --quiet
+
+# Install helm
+wget "https://storage.googleapis.com/kubernetes-helm/helm-v${HELM_VERSION}-linux-amd64.tar.gz"
+tar --extract --file "helm-v${HELM_VERSION}-linux-amd64.tar.gz"
+sudo mv linux-amd64/helm /usr/local/bin
 
 # Install node
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
