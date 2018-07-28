6043eb5fd84862535f095cb127a1fc70040bf67a
Andrew Howden <hello@andrewhowden.com>
2018-07-28 18:01:02 +0200

    AD-HOC fix (build): Remove redundant secrets configuration

--- ci/scripts/unpack-secrets.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-# Decrypt the tar file
-openssl aes-256-cbc -K $encrypted_33efbe80436a_key -iv $encrypted_33efbe80436a_iv -in .secrets.tar.gz.enc -out .secrets.tar.gz -d
-
-# Unpack the file
-tar -xvf .secrets.tar.gz
